daf87fc51b9362909a0bc0c2ebec3f9c0f501128
yr2314 <yr2314@tc.columbia.edu>
2016-11-10 01:10:50 -0500

    debug

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ function whocare() {
     document.getElementById("AnswerBox").innerHTML = '&nbspRight!&nbsp';
     document.getElementById("AnswerBox").style.visibility = "visible";
     document.getElementById("Why").style.visibility = "visible";
+    document.getElementById("next").style.visibility = "visible";
 }
 
 function death() {
@@ -14,6 +15,7 @@ function coldlaugh() {
     document.getElementById("AnswerBox2").innerHTML = '&nbspRight!&nbsp';
     document.getElementById("AnswerBox2").style.visibility = "visible";
     document.getElementById("Why2").style.visibility = "visible";
+    document.getElementById("moretest").style.visibility = "visible";
 }
 
 function hehe() {
@@ -22,9 +24,9 @@ function hehe() {
     document.getElementById("Why2").style.visibility = "visible";
 }
 function webpage() {
-  if(document.getElementById("search").value=='trump'){
+  if(document.getElementById("#search").value=='trump'){
     window.open("https://www.donaldjtrump.com/");
-  }else if(document.getElementById("search").value=='hillary'){
+  }else if(document.getElementById("#search").value=='hillary'){
     window.open("https://www.hillaryclinton.com/");
   }else{
     var x=document.querySelector("#search").value;
@@ -36,18 +38,18 @@ function nexttest(){
   document.getElementById("testtest2").style.visibility = "visible";
 }
 function moretest(){
-  alert("Sorry, the programer is too tired to get another one...");
+  alert("Type in keywords in search engine and search more!");
 }
 
 function hillaryVote() {
-    if (confirm("Are you sure you want to vote for Trump?")) {
-        if (confirm("Did you notice about his facial expression?")) {
-            alert("Facial expression is a very important part expressing his feeling");
+    if (confirm("Are you sure you want to vote for Hillary?")) {
+        if (confirm("Did you notice about her facial expression?")) {
+            alert("Facial expression is a very important part expressing her truly thought.");
         } else {
-            alert("Facial expression is transforming his words");
+            alert("Facial expression is expressing her thoughts.");
         }
     } else {
-        alert("Do you trully understand his words? Remember to see his facial exprssion");
+        alert("Do you trully understand her words? Pay attention to her facial exprssion.");
     }
 
 }
@@ -55,12 +57,12 @@ function hillaryVote() {
 function trumpVote() {
     if (confirm("Are you sure you want to vote for Trump?")) {
         if (confirm("Did you notice about his facial expression?")) {
-            alert("Facial expression is a very important part expressing his feeling");
+            alert("Facial expression is a very important part expressing his truly thought.");
         } else {
-            alert("Facial expression is transforming his words");
+            alert("Facial expression is expressing his thoughts.");
         }
     } else {
-        alert("Do you trully understand his words? Remember to see his facial exprssion");
+        alert("Do you trully understand her words? Pay attention to her facial exprssion.");
     }
 
 }
